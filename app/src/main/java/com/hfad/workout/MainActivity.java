cb513aead08a10883c48bb579055e4291d52ff96
OlgaZ <olga.z.kordik@gmail.com>
2019-07-26 18:10:21 +0300

    Connected WorkoutListFragment with MainActivity

--- app/src/main/java/com/hfad/workout/MainActivity.java
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -3,9 +3,8 @@ package com.hfad.workout;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onShowDetails(View view) {
+    @Override
+    public void itemClicked(long id) {
         Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
         startActivity(intent);
     }
 }
