8165ff74c7b9911d14d2bc2896feb605c7f6f857
OlgaZ <olga.z.kordik@gmail.com>
2019-07-26 17:34:22 +0300

    Added WorkoutListFragment

---
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -0,0 +1,19 @@
+package com.hfad.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class WorkoutListFragment extends ListFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
